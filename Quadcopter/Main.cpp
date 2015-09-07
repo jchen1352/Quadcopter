cc76ebb2100f9dfa1c5f715c208f4b996cee7d53
Jeffrey <jchen1352@gmail.com>
2015-09-06 23:18:02 -0700

    Definitions

--- Quadcopter/Main.cpp
+++ Quadcopter/Main.cpp
@@ -1,9 +1,13 @@
 #include "stdafx.h"
+#include <iostream>
 #include "Quadcopter.h"
-
-using namespace std;
 
 int main()
 {
+	uint8_t pitch = getPitch();
+	uint8_t roll = getRoll();
+	uint8_t height = getHeight();
+	uint8_t FR = 0, FL = 0, BR = 0, BL = 0;
+
     return 0;
 }
--- Quadcopter/Quadcopter.cpp
+++ Quadcopter/Quadcopter.cpp
@@ -1,2 +1,25 @@
+#include "stdafx.h"
+#include <iostream>
 #include "Quadcopter.h"
 
+int8_t getPitch() {
+	int8_t pitch;
+	std::cin >> pitch;
+	return pitch;
+}
+
+int8_t getRoll() {
+	int8_t roll;
+	std::cin >> roll;
+	return roll;
+}
+
+uint8_t getHeight() {
+	uint8_t height;
+	std::cin >> height;
+	return height;
+}
+
+void setThrust(uint8_t& motor, uint8_t strength) {
+	motor = strength;
+}
